6152735253b288999350fa59b36f9e483999529d
Gael Sonney <gael.sonney@siteminder.com>
2016-09-15 16:24:07 +1000

    rename

--- fix_tests_import.sh
+++ fix_tests_import.sh
@@ -4,4 +4,4 @@ grep -r -l 'au.com.siteminder.model.test.SiteminderModelDefinitions' . | sort | 
 grep -r -l 'au.com.siteminder.model.test.ModelFactory' . | sort | uniq | xargs perl -e "s/au.com.siteminder.model.test.ModelFactory/com.siteminder.test.ModelFactory/" -pi;
 grep -r -l 'new SiteminderModelDefinitions().initialize()' . | sort | uniq | xargs perl -e "s/SiteminderModelDefinitions\(\).initialize/ChannelManagerModelDefinitions\(\).initialize/" -pi;
 
-
+echo 'Done'
